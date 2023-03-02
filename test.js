c3dbef4b598dc0165bea9c5c66ae340e5fa84c30
matt20-creator <55395017+matt20-creator@users.noreply.github.com>
2023-03-03 00:36:44 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
 if(!window.location.href.contains("https://www.suedtirolmobil.info/") != -1){
     document.getElementById('keyboardDragger').style.display = 'none'
 }
 
-console.log('test')
+console.log('test')    
+});
